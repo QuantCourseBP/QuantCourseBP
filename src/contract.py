e96597a1b528cefa2e40aa2706c900f3cdd01cb5
Friedmann, Gabor <friedmanngabor93@gmail.com>
2023-03-12 22:55:16 +0100

    Refactor contracts

--- src/contract.py
+++ src/contract.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from enums import *
-import numpy as np
 
 
 class Contract(ABC):
-    _underlying: Stock
-    _derivative_type: PutCallFwd
-    _strike: float
-    _expiry: float
+    def __init__(self, und: Stock, dtype: PutCallFwd, strk: float, exp: float) -> None:
+        self._underlying: Stock = und
+        self._derivative_type: PutCallFwd = dtype
+        self._strike: float = strk
+        self._expiry: float = exp
 
     def get_und(self) -> Stock:
         return self._underlying
@@ -22,9 +22,17 @@ class Contract(ABC):
     def get_expiry(self) -> float:
         return self._expiry
 
+    def __str__(self) -> str:
+        return str(self.to_dict())
+
     @abstractmethod
-    def display(self) -> dict[str, any]:
-        pass
+    def to_dict(self) -> dict[str, any]:
+        return {
+            "underlying": self._underlying.value,
+            "type": self._derivative_type.value,
+            "strike": self._strike,
+            "expiry": self._expiry
+        }
 
     @abstractmethod
     def convert_to_generic(self) -> GenericContract:
@@ -34,8 +42,14 @@ class Contract(ABC):
     def get_timeline(self) -> list[float]:
         pass
 
+    def _raise_incorrect_derivative_type(self):
+        raise TypeError(f'Derivative type of {type(self).__name__} must be CALL or PUT')
+
 
 class VanillaContract(Contract):
+    def to_dict(self) -> dict[str, any]:
+        return super().to_dict()
+
     @abstractmethod
     def convert_to_generic(self) -> GenericContract:
         pass
@@ -43,20 +57,6 @@ class VanillaContract(Contract):
     @abstractmethod
     def payoff(self, spot: float) -> float:
         pass
-
-    def __init__(self, und: Stock, dtype: PutCallFwd, strk: float, exp: float) -> None:
-        self._underlying = und
-        self._derivative_type = dtype
-        self._strike = strk
-        self._expiry = exp
-
-    def display(self) -> dict[str, any]:
-        return {
-            "underlying": self._underlying.value,
-            "type": self._derivative_type.value,
-            "strike": self._strike,
-            "expiry": self._expiry
-        }
 
     def get_timeline(self) -> list[float]:
         return [self._expiry]
@@ -74,12 +74,22 @@ class ForwardContract(VanillaContract):
         return spot - self._strike
 
 
-class EuropeanContract(VanillaContract):
+class VanillaOptionContract(VanillaContract):
     def __init__(self, und: Stock, dtype: PutCallFwd, strk: float, exp: float) -> None:
         if dtype not in [PutCallFwd.CALL, PutCallFwd.PUT]:
-            raise TypeError(f'Derivative type of {type(self).__name__} must be CALL or PUT')
+            self._raise_incorrect_derivative_type()
         super().__init__(und, dtype, strk, exp)
 
+    @abstractmethod
+    def convert_to_generic(self) -> GenericContract:
+        pass
+
+    @abstractmethod
+    def payoff(self, spot: float) -> float:
+        pass
+
+
+class EuropeanContract(VanillaOptionContract):
     def convert_to_generic(self) -> GenericContract:
         # todo: needs to be properly initialized after GenericContract is implemented
         return GenericContract()
@@ -90,15 +100,10 @@ class EuropeanContract(VanillaContract):
         elif self._derivative_type == PutCallFwd.PUT:
             return max(self._strike - spot, 0)
         else:
-            return np.NaN
+            self._raise_incorrect_derivative_type()
 
 
-class AmericanContract(VanillaContract):
-    def __init__(self, und: Stock, dtype: PutCallFwd, strk: float, exp: float) -> None:
-        if dtype not in [PutCallFwd.CALL, PutCallFwd.PUT]:
-            raise TypeError(f'Derivative type of {type(self).__name__} must be CALL or PUT')
-        super().__init__(und, dtype, strk, exp)
-
+class AmericanContract(VanillaOptionContract):
     def convert_to_generic(self) -> GenericContract:
         # todo: needs to be properly initialized after GenericContract is implemented
         return GenericContract()
@@ -109,15 +114,10 @@ class AmericanContract(VanillaContract):
         elif self._derivative_type == PutCallFwd.PUT:
             return max(self._strike - spot, 0)
         else:
-            return np.NaN
+            self._raise_incorrect_derivative_type()
 
 
-class EuropeanDigitalContract(VanillaContract):
-    def __init__(self, und: Stock, dtype: PutCallFwd, strk: float, exp: float) -> None:
-        if dtype not in [PutCallFwd.CALL, PutCallFwd.PUT]:
-            raise TypeError(f'Derivative type of {type(self).__name__} must be CALL or PUT')
-        super().__init__(und, dtype, strk, exp)
-
+class EuropeanDigitalContract(VanillaOptionContract):
     def convert_to_generic(self) -> GenericContract:
         # todo: needs to be properly initialized after GenericContract is implemented
         return GenericContract()
@@ -128,7 +128,7 @@ class EuropeanDigitalContract(VanillaContract):
         elif self._derivative_type == PutCallFwd.PUT:
             return float(self._strike - spot > 0)
         else:
-            return np.NaN
+            self._raise_incorrect_derivative_type()
 
 
 class GenericContract:
