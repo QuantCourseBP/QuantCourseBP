64ef2bf691e9b4eb8d5de3428f1adba1729aa400
vargala4 <vargala4@gmail.com>
2023-04-23 21:09:46 +0200

    - AsianContract definition
    - Testing contracts

--- src/contract.py
+++ src/contract.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from src.enums import *
+from statistics import mean
 
 
 class Contract(ABC):
-    def __init__(self, und: Stock, dtype: PutCallFwd, strk: float, exp: float) -> None:
+    def __init__(self, und: Stock, dtype: PutCallFwd, strk: float, exp: float, num_mon: int = 1) -> None:
         self._underlying: Stock = und
         self._derivative_type: PutCallFwd = dtype
         self._strike: float = strk
         self._expiry: float = exp
+        self._num_mon: int = num_mon    # Asian: nr of averaging points; Barrier: nr of monitoring points
 
     def get_und(self) -> Stock:
         return self._underlying
@@ -34,9 +36,9 @@ class Contract(ABC):
             "expiry": self._expiry
         }
 
-    @abstractmethod
-    def convert_to_generic(self) -> GenericContract:
-        pass
+    # @abstractmethod
+    # def convert_to_generic(self) -> GenericContract:
+    #     pass
 
     @abstractmethod
     def get_timeline(self) -> list[float]:
@@ -50,9 +52,9 @@ class VanillaContract(Contract):
     def to_dict(self) -> dict[str, any]:
         return super().to_dict()
 
-    @abstractmethod
-    def convert_to_generic(self) -> GenericContract:
-        pass
+    # @abstractmethod
+    # def convert_to_generic(self) -> GenericContract:
+    #     pass
 
     @abstractmethod
     def payoff(self, spot: float) -> float:
@@ -66,9 +68,9 @@ class ForwardContract(VanillaContract):
     def __init__(self, und: Stock, strk: float, exp: float) -> None:
         super().__init__(und, PutCallFwd.FWD, strk, exp)
 
-    def convert_to_generic(self) -> GenericContract:
-        # todo: needs to be properly initialized after GenericContract is implemented
-        return GenericContract()
+    # def convert_to_generic(self) -> GenericContract:
+    #     # todo: needs to be properly initialized after GenericContract is implemented
+    #     return GenericContract()
 
     def payoff(self, spot: float) -> float:
         return spot - self._strike
@@ -80,9 +82,9 @@ class VanillaOptionContract(VanillaContract):
             self._raise_incorrect_derivative_type()
         super().__init__(und, dtype, strk, exp)
 
-    @abstractmethod
-    def convert_to_generic(self) -> GenericContract:
-        pass
+    # @abstractmethod
+    # def convert_to_generic(self) -> GenericContract:
+    #     pass
 
     @abstractmethod
     def payoff(self, spot: float) -> float:
@@ -90,9 +92,9 @@ class VanillaOptionContract(VanillaContract):
 
 
 class EuropeanContract(VanillaOptionContract):
-    def convert_to_generic(self) -> GenericContract:
-        # todo: needs to be properly initialized after GenericContract is implemented
-        return GenericContract()
+    # def convert_to_generic(self) -> GenericContract:
+    #     # todo: needs to be properly initialized after GenericContract is implemented
+    #     return GenericContract()
 
     def payoff(self, spot: float) -> float:
         if self._derivative_type == PutCallFwd.CALL:
@@ -104,9 +106,9 @@ class EuropeanContract(VanillaOptionContract):
 
 
 class AmericanContract(VanillaOptionContract):
-    def convert_to_generic(self) -> GenericContract:
-        # todo: needs to be properly initialized after GenericContract is implemented
-        return GenericContract()
+    # def convert_to_generic(self) -> GenericContract:
+    #     # todo: needs to be properly initialized after GenericContract is implemented
+    #     return GenericContract()
 
     def payoff(self, spot: float) -> float:
         if self._derivative_type == PutCallFwd.CALL:
@@ -118,9 +120,9 @@ class AmericanContract(VanillaOptionContract):
 
 
 class EuropeanDigitalContract(VanillaOptionContract):
-    def convert_to_generic(self) -> GenericContract:
-        # todo: needs to be properly initialized after GenericContract is implemented
-        return GenericContract()
+    # def convert_to_generic(self) -> GenericContract:
+    #     # todo: needs to be properly initialized after GenericContract is implemented
+    #     return GenericContract()
 
     def payoff(self, spot: float) -> float:
         if self._derivative_type == PutCallFwd.CALL:
@@ -131,6 +133,84 @@ class EuropeanDigitalContract(VanillaOptionContract):
             self._raise_incorrect_derivative_type()
 
 
-class GenericContract:
+class PathDependentContract(Contract):
+
+    def to_dict(self) -> dict[str, any]:
+        return super().to_dict()
+
+    # @abstractmethod
+    # def convert_to_generic(self) -> GenericContract:
+    #     pass
+
+    @abstractmethod
+    def payoff(self, spot: float) -> float:
+        pass
+
+    def get_timeline(self) -> list[float]:
+        return [((i+1)/self._num_mon)*self._expiry for i in range(self._num_mon)]
+
+
+class AsianContract(PathDependentContract):
+
+    # def convert_to_generic(self) -> GenericContract:
+    #     # todo: needs to be properly initialized after GenericContract is implemented
+    #     return GenericContract()
+
+    def payoff(self, prices_und: float) -> float:
+    # TO DO: prices_und to derive from the underlying process using the timeline
+        if self._derivative_type == PutCallFwd.CALL:
+            return max(mean(prices_und) - self._strike, 0)
+        elif self._derivative_type == PutCallFwd.PUT:
+            return max(self._strike - mean(prices_und), 0)
+        else:
+            self._raise_incorrect_derivative_type()
+
+
+class EuropeanBarrierContract(PathDependentContract):
+
+    # def convert_to_generic(self) -> GenericContract:
+    #     # todo: needs to be properly initialized after GenericContract is implemented
+    #     return GenericContract()
+
+    def payoff(self, spot: float) -> float:
+        if self._derivative_type == PutCallFwd.CALL:
+            return max(spot - self._strike, 0)
+        elif self._derivative_type == PutCallFwd.PUT:
+            return max(self._strike - spot, 0)
+        else:
+            self._raise_incorrect_derivative_type()
+
+
+class GenericContract(PathDependentContract):
     # todo: needs to be implemented, defined temporarily as empty class due to annotations
     pass
+
+    def payoff(self, spot: float) -> float:
+        if self._derivative_type == PutCallFwd.CALL:
+            return max(spot - self._strike, 0)
+        elif self._derivative_type == PutCallFwd.PUT:
+            return max(self._strike - spot, 0)
+        else:
+            self._raise_incorrect_derivative_type()
+
+
+def main():
+
+    trade1 = ForwardContract('Apple', 1, 2)
+    print(trade1.get_und())
+    print(trade1.get_type())
+    print("Strike: {}".format(trade1.get_strike()))
+    print(trade1.payoff(0.5))
+    print(trade1.payoff(1.5))
+
+    print("\n")
+    trade2 = AsianContract('Microsoft', 'CALL', 1, 2)
+    print(trade2.get_und())
+    print(trade2.get_type())
+    print("Strike: {}".format(trade2.get_strike()))
+    print(trade2.payoff([1.8, 1.9, 2, 1.9, 2.1]))
+    print(trade2.payoff([0.8, 0.9, 1, 0.9, 1.1]))
+
+
+if __name__ == '__main__': main()
+
