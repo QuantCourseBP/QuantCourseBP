0e844cece921d968756c834735d86cefbd9612c1
DESKTOP-TLP8FD7\Solymosi Ernő <solymosierno@gmail.com>
2023-05-21 21:44:01 +0200

    move MC numerical method

--- src/numerical_method.py
+++ src/numerical_method.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from abc import ABC
 from src.model import *
+import numpy as np
 
 
 class NumericalMethod(ABC):
@@ -13,23 +14,58 @@ class NumericalMethod(ABC):
         return {cls.__name__: cls for cls in NumericalMethod.__subclasses__()}
 
 
-# todo: to be implemented
 class MCMethod(NumericalMethod):
     def __init__(self, model: MarketModel, params: MCParams):
         if not isinstance(params, MCParams):
             raise TypeError('Params must be an instance of class MCParams')
         super().__init__(model, params)
 
+    def generate_std_norm(self) -> np.array:
+        np.random.seed(self._params.num_of_paths)
+        num_of_tenors = len(self._params.tenors)
+        if self._params.antithetic:
+            rnd1 = np.random.standard_normal(size=(int(self._params.num_of_paths / 2), num_of_tenors))
+            rnd2 = -rnd1
+            rnd = np.concatenate((rnd1, rnd2), axis=0)
+            if self._params.num_of_paths % 2 == 1:
+                zeros = np.zeros((1, num_of_tenors))
+                rnd = np.concatenate((rnd, zeros), axis=0)
+        else:
+            rnd = np.random.standard_normal(size=(self._params.num_of_paths, num_of_tenors))
+        if self._params.standardize:
+            mean = np.mean(rnd)
+            std = np.std(rnd)
+            rnd = (rnd - mean) / std
+        return rnd
 
-# todo: to be implemented
-class PDEMethod(NumericalMethod):
-    def __init__(self, model: MarketModel, params: PDEParams):
-        if not isinstance(params, PDEParams):
-            raise TypeError('Params must be an instance of class PDEParams')
+    @abstractmethod
+    def create_spot_paths(self) -> np.array:
+        pass
+
+
+class MCMethodBS(MCMethod):
+    def __init__(self, model: BSVolModel, params: MCParams):
+        if not isinstance(params, MCParams):
+            raise TypeError('Params must be an instance of class MCParams')
         super().__init__(model, params)
 
+    def create_spot_paths(self) -> np.array:
+        std_norm = self.generate_std_norm()
+        rf_rate = self._model.get_rate()
+        vol = self._model.get_vol()
+        log_proc_drift = rf_rate - 1 / 2 * vol * vol
+        s0 = self._model.get_initial_spot()
+        num_of_tenors = len(self._params.tenors)
+        log_ret_paths = np.zeros((self._params.num_of_paths, num_of_tenors + 1))
+        for path in range(self._params.num_of_paths):
+            for tenor_idx in range(1, num_of_tenors + 1):
+                d_time = self._params.tenors[tenor_idx] - self._params.tenors[tenor_idx - 1]
+                log_ret_paths[path][tenor_idx] = log_ret_paths[path][
+                                                     tenor_idx - 1] + log_proc_drift * d_time + vol * np.sqrt(d_time) * \
+                                                 std_norm[path][tenor_idx - 1]
+        return s0 * np.exp(log_ret_paths)
 
-# todo: to be implemented
+
 class TreeMethod(NumericalMethod):
     def __init__(self, model: MarketModel, params: TreeParams):
         if not isinstance(params, TreeParams):
@@ -48,11 +84,12 @@ class Params(ABC):
 
 
 class MCParams(Params):
-    def __init__(self) -> None:
-        # todo: to be implemented, a few examples:
-        self.seed: int = 0
-        self.num_of_paths: int = 100
-        self.timestep: int = 10
+    def __init__(self, **kwargs) -> None:
+        self.seed: int = kwargs["seed"] if "seed" in kwargs else 0
+        self.num_of_paths: int = kwargs["num_of_paths"] if "num_of_paths" in kwargs else 100
+        self.tenors: list[float] = kwargs["tenors"] if "tenors" in kwargs else [0,1]
+        self.antithetic: bool = kwargs["antithetic"] if "antithetic" in kwargs else True
+        self.standardize: bool = kwargs["standardize"] if "standardize" in kwargs else True
 
 
 class PDEParams(Params):
