a999118648286724f628616a2d8d625374007d80
Friedmann, Gabor <friedmanngabor93@gmail.com>
2023-07-25 01:17:02 +0200

    Move bump and reval greek implementation up into Pricer base class

--- src/pricer.py
+++ src/pricer.py
@@ -24,25 +24,77 @@ class Pricer(ABC):
     def calc_fair_value(self) -> float:
         pass
 
-    @abstractmethod
     def calc_delta(self, method: GreekMethod) -> float:
-        pass
+        if method != GreekMethod.BUMP:
+            self._raise_unsupported_greek_method_error(method, (GreekMethod.BUMP,))
+        bump_size = self.RELATIVE_BUMP_SIZE * self._model.get_initial_spot()
+        bumped_fair_values = list()
+        for b in (bump_size, -bump_size):
+            model = copy.deepcopy(self._model)
+            model.bump_initial_spot(b)
+            bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
+            bumped_fair_values.append(bumped_pricer.calc_fair_value())
+            del bumped_pricer
+            del model
+        return (bumped_fair_values[0] - bumped_fair_values[1]) / (2 * bump_size)
 
-    @abstractmethod
     def calc_gamma(self, method: GreekMethod) -> float:
-        pass
+        if method != GreekMethod.BUMP:
+            self._raise_unsupported_greek_method_error(method, (GreekMethod.BUMP,))
+        bump_size = self.RELATIVE_BUMP_SIZE * self._model.get_initial_spot()
+        bumped_fair_values = list()
+        for b in (bump_size, -bump_size):
+            model = copy.deepcopy(self._model)
+            model.bump_initial_spot(b)
+            bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
+            bumped_fair_values.append(bumped_pricer.calc_fair_value())
+            del bumped_pricer
+            del model
+        return (bumped_fair_values[0] - 2 * self.calc_fair_value() + bumped_fair_values[1]) / (bump_size ** 2)
 
-    @abstractmethod
     def calc_vega(self, method: GreekMethod) -> float:
-        pass
+        if method != GreekMethod.BUMP:
+            self._raise_unsupported_greek_method_error(method, (GreekMethod.BUMP,))
+        spot = self._model.get_initial_spot()
+        strike = self._contract.get_strike()
+        tenor = self._contract.get_expiry()
+        vol = self._model.get_volgrid().get_vol(tenor, spot / strike)
+        bump_size = self.RELATIVE_BUMP_SIZE * vol
+        bumped_fair_values = list()
+        for b in (bump_size, -bump_size):
+            model = copy.deepcopy(self._model)
+            model.bump_volgrid(b)
+            bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
+            bumped_fair_values.append(bumped_pricer.calc_fair_value())
+            del bumped_pricer
+            del model
+        return (bumped_fair_values[0] - bumped_fair_values[1]) / (2 * bump_size)
 
-    @abstractmethod
     def calc_theta(self, method: GreekMethod) -> float:
-        pass
+        if method != GreekMethod.BUMP:
+            self._raise_unsupported_greek_method_error(method, (GreekMethod.BUMP,))
+        bump_size = 1.0 / 252.0
+        contract = copy.deepcopy(self._contract)
+        contract.set_expiry(self._contract.get_expiry() + bump_size)
+        bumped_pricer = globals()[type(self).__name__](contract, self._model, self._method)
+        bumped_fair_value = bumped_pricer.calc_fair_value()
+        del bumped_pricer
+        del contract
+        return -1.0 * (bumped_fair_value - self.calc_fair_value()) / bump_size
 
-    @abstractmethod
     def calc_rho(self, method: GreekMethod) -> float:
-        pass
+        if method != GreekMethod.BUMP:
+            self._raise_unsupported_greek_method_error(method, (GreekMethod.BUMP,))
+        bump_size = self.RELATIVE_BUMP_SIZE * self._model.get_rate()
+        bumped_fair_values = list()
+        for b in (bump_size, -bump_size):
+            model = copy.deepcopy(self._model)
+            model.bump_rate(b)
+            bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
+            bumped_fair_values.append(bumped_pricer.calc_fair_value())
+            del bumped_pricer
+            del model
+        return (bumped_fair_values[0] - bumped_fair_values[1]) / (2 * bump_size)
 
     def _raise_unsupported_greek_method_error(
             self,
@@ -74,8 +126,7 @@ class EuropeanAnalyticPricer(Pricer):
         strike = self._contract.get_strike()
         tenor = self._contract.get_expiry()
         if tenor < 1e-8:
-            # return zero if option expired
-            return 0.0
+            return 0.0  # return zero if option expired
         spot = self._model.get_initial_spot()
         vol = self._model.get_vol(tenor, spot / strike)
         rate = self._model.get_rate()
@@ -90,10 +141,9 @@ class EuropeanAnalyticPricer(Pricer):
             self.__raise_incorrect_derivative_type_error()
 
     def calc_delta(self, method: GreekMethod) -> float:
-        spot = self._model.get_initial_spot()
         if method == GreekMethod.ANALYTIC:
             greek = 0.0
-            direction = self._contract.get_direction()
+            spot = self._model.get_initial_spot()
             strike = self._contract.get_strike()
             tenor = self._contract.get_expiry()
             vol = self._model.get_volgrid().get_vol(tenor, spot / strike)
@@ -105,87 +155,57 @@ class EuropeanAnalyticPricer(Pricer):
                 greek = -norm.cdf(-d1)
             else:
                 self.__raise_incorrect_derivative_type_error()
-            return direction * greek
+            return self._contract.get_direction() * greek
         elif method == GreekMethod.BUMP:
-            bump_size = self.RELATIVE_BUMP_SIZE * spot
-            bumped_fair_values = list()
-            for b in (bump_size, -bump_size):
-                model = copy.deepcopy(self._model)
-                model.bump_initial_spot(b)
-                bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
-                bumped_fair_values.append(bumped_pricer.calc_fair_value())
-                del bumped_pricer
-                del model
-            return (bumped_fair_values[0] - bumped_fair_values[1]) / (2 * bump_size)
+            return super().calc_delta(method)
         else:
             self._raise_unsupported_greek_method_error(method)
 
     def calc_gamma(self, method: GreekMethod) -> float:
-        spot = self._model.get_initial_spot()
         if method == GreekMethod.ANALYTIC:
             greek = 0.0
-            direction = self._contract.get_direction()
+            spot = self._model.get_initial_spot()
             strike = self._contract.get_strike()
             tenor = self._contract.get_expiry()
             vol = self._model.get_volgrid().get_vol(tenor, spot / strike)
             rate = self._model.get_rate()
+            d1 = EuropeanAnalyticPricer.__d1(spot, strike, vol, rate, tenor)
             if self._contract.get_type() in (PutCallFwd.CALL, PutCallFwd.PUT):
-                d1 = EuropeanAnalyticPricer.__d1(spot, strike, vol, rate, tenor)
                 greek = norm.pdf(d1) / (spot * vol * np.sqrt(tenor))
             else:
                 self.__raise_incorrect_derivative_type_error()
-            return direction * greek
+            return self._contract.get_direction() * greek
         elif method == GreekMethod.BUMP:
-            bump_size = self.RELATIVE_BUMP_SIZE * spot
-            bumped_fair_values = list()
-            for b in (bump_size, -bump_size):
-                model = copy.deepcopy(self._model)
-                model.bump_initial_spot(b)
-                bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
-                bumped_fair_values.append(bumped_pricer.calc_fair_value())
-                del bumped_pricer
-                del model
-            return (bumped_fair_values[0] - 2 * self.calc_fair_value() + bumped_fair_values[1]) / (bump_size ** 2)
+            return super().calc_gamma(method)
         else:
             self._raise_unsupported_greek_method_error(method)
 
     def calc_vega(self, method: GreekMethod) -> float:
-        spot = self._model.get_initial_spot()
-        strike = self._contract.get_strike()
-        tenor = self._contract.get_expiry()
-        vol = self._model.get_volgrid().get_vol(tenor, spot / strike)
         if method == GreekMethod.ANALYTIC:
             greek = 0.0
-            direction = self._contract.get_direction()
+            spot = self._model.get_initial_spot()
+            strike = self._contract.get_strike()
+            tenor = self._contract.get_expiry()
+            vol = self._model.get_volgrid().get_vol(tenor, spot / strike)
             rate = self._model.get_rate()
             df = self._model.get_df(tenor)
+            d2 = EuropeanAnalyticPricer.__d2(spot, strike, vol, rate, tenor)
             if self._contract.get_type() in (PutCallFwd.CALL, PutCallFwd.PUT):
-                d2 = EuropeanAnalyticPricer.__d2(spot, strike, vol, rate, tenor)
                 greek = strike * df * norm.pdf(d2) * np.sqrt(tenor)
             else:
                 self.__raise_incorrect_derivative_type_error()
-            return direction * greek
+            return self._contract.get_direction() * greek
         elif method == GreekMethod.BUMP:
-            bump_size = self.RELATIVE_BUMP_SIZE * vol
-            bumped_fair_values = list()
-            for b in (bump_size, -bump_size):
-                model = copy.deepcopy(self._model)
-                model.bump_volgrid(b)
-                bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
-                bumped_fair_values.append(bumped_pricer.calc_fair_value())
-                del bumped_pricer
-                del model
-            return (bumped_fair_values[0] - bumped_fair_values[1]) / (2 * bump_size)
+            return super().calc_vega(method)
         else:
             self._raise_unsupported_greek_method_error(method)
 
     def calc_theta(self, method: GreekMethod) -> float:
-        tenor = self._contract.get_expiry()
         if method == GreekMethod.ANALYTIC:
             greek = 0.0
-            direction = self._contract.get_direction()
             strike = self._contract.get_strike()
             spot = self._model.get_initial_spot()
+            tenor = self._contract.get_expiry()
             vol = self._model.get_volgrid().get_vol(tenor, spot / strike)
             rate = self._model.get_rate()
             df = self._model.get_df(tenor)
@@ -197,27 +217,20 @@ class EuropeanAnalyticPricer(Pricer):
                 greek = -1.0 * ((spot * norm.pdf(d1) * vol) / (2 * np.sqrt(tenor)) - rate * strike * df * norm.cdf(-d2))
             else:
                 self.__raise_incorrect_derivative_type_error()
-            return direction * greek
+            return self._contract.get_direction() * greek
         elif method == GreekMethod.BUMP:
-            bump_size = 1.0 / 252.0
-            contract = copy.deepcopy(self._contract)
-            contract.set_expiry(tenor + bump_size)
-            bumped_pricer = globals()[type(self).__name__](contract, self._model, self._method)
-            bumped_fair_value = bumped_pricer.calc_fair_value()
-            del bumped_pricer
-            return -1.0 * (bumped_fair_value - self.calc_fair_value()) / bump_size
+            return super().calc_theta(method)
         else:
             self._raise_unsupported_greek_method_error(method)
 
     def calc_rho(self, method: GreekMethod) -> float:
-        rate = self._model.get_rate()
         if method == GreekMethod.ANALYTIC:
             greek = 0.0
-            direction = self._contract.get_direction()
             strike = self._contract.get_strike()
             tenor = self._contract.get_expiry()
             spot = self._model.get_initial_spot()
             vol = self._model.get_volgrid().get_vol(tenor, spot / strike)
+            rate = self._model.get_rate()
             df = self._model.get_df(tenor)
             d2 = EuropeanAnalyticPricer.__d2(spot, strike, vol, rate, tenor)
             if self._contract.get_type() == PutCallFwd.CALL:
@@ -226,18 +239,9 @@ class EuropeanAnalyticPricer(Pricer):
                 greek = -strike * tenor * df * norm.cdf(-d2)
             else:
                 self.__raise_incorrect_derivative_type_error()
-            return direction * greek
+            return self._contract.get_direction() * greek
         elif method == GreekMethod.BUMP:
-            bump_size = self.RELATIVE_BUMP_SIZE * rate
-            bumped_fair_values = list()
-            for b in (bump_size, -bump_size):
-                model = copy.deepcopy(self._model)
-                model.bump_rate(b)
-                bumped_pricer = globals()[type(self).__name__](self._contract, model, self._method)
-                bumped_fair_values.append(bumped_pricer.calc_fair_value())
-                del bumped_pricer
-                del model
-            return (bumped_fair_values[0] - bumped_fair_values[1]) / (2 * bump_size)
+            return super().calc_rho(method)
         else:
             self._raise_unsupported_greek_method_error(method)
 
