f7ed6e62c6cbdfa2236a9b654f1588d60010a1e4
solymosi-erno <solymosierno@gmail.com>
2023-08-06 09:33:34 +0200

    move mc method to numerical_method

--- src/numerical_method.py
+++ src/numerical_method.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from abc import ABC
 from src.model import *
+import numpy as np
 
 
 class NumericalMethod(ABC):
@@ -20,6 +21,54 @@ class MCMethod(NumericalMethod):
             raise TypeError('Params must be an instance of class MCParams')
         super().__init__(model, params)
 
+    def generate_std_norm(self) -> np.array:
+
+        np.random.seed(self._params.seed)
+
+        if self._params.antithetic:
+            rnd1 = np.random.standard_normal(size=(int(self._params.num_of_paths / 2), self._params.num_of_tenors))
+            rnd2 = -rnd1
+            rnd = np.concatenate((rnd1, rnd2), axis=0)
+            if self._params.num_of_paths % 2 == 1:
+                zeros = np.zeros((1, self._params.num_of_tenors))
+                rnd = np.concatenate((rnd, zeros), axis=0)
+
+        else:
+            rnd = np.random.standard_normal(size=(self._params.num_of_paths, self._params.num_of_tenors))
+
+        if self._params.standardize:
+            mean = np.mean(rnd)
+            std = np.std(rnd)
+            rnd = (rnd - mean) / std
+        return rnd
+
+
+class MCMethodFlatVol(MCMethod):
+    def __init__(self, model: FlatVolModel, params: MCParams):
+        super().__init__(model, params)
+
+    def create_spot_paths(self) -> np.array:
+
+        std_norm = self.generate_std_norm()
+
+        rf_rate = self._model.get_rate()
+        vol = self._model.vol
+        log_proc_drift = rf_rate - 1 / 2 * vol * vol
+
+        s0 = self.index.level
+
+        log_ret_paths = np.zeros((self.num_of_paths, self.num_of_tenors + 1))
+
+        for path in range(self.num_of_paths):
+            log_ret_paths[path][0] = 0
+
+        for path in range(self.num_of_paths):
+            for tenor_idx in range(1, self.num_of_tenors + 1):
+                d_time = self.tenors[tenor_idx] - self.tenors[tenor_idx - 1]
+                log_ret_paths[path][tenor_idx] = log_ret_paths[path][
+                                                     tenor_idx - 1] + log_proc_drift * d_time + vol * np.sqrt(d_time) * \
+                                                 std_norm[path][tenor_idx - 1]
+        return s0 * np.exp(log_ret_paths)
 
 # todo: to be implemented
 class PDEMethod(NumericalMethod):
@@ -52,7 +101,9 @@ class MCParams(Params):
         # todo: to be implemented, a few examples:
         self.seed: int = 0
         self.num_of_paths: int = 100
-        self.timestep: int = 10
+        self.tenors: np.array = [1]
+        self.standardize: bool = True
+        self.antithetic: bool = True
 
 
 class PDEParams(Params):
