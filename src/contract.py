4290cf9ac566b158cf7a5ec5f804117b514959d5
Friedmann, Gabor <friedmanngabor93@gmail.com>
2023-09-07 00:08:22 +0200

    Remove unnecessary GenericContract

--- src/contract.py
+++ src/contract.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from src.enums import *
 from statistics import mean
-import numpy as np
 
 
 class Contract(ABC):
@@ -39,10 +38,6 @@ class Contract(ABC):
         }
 
     @abstractmethod
-    def convert_to_generic(self) -> GenericContract:
-        pass
-
-    @abstractmethod
     def get_timeline(self) -> list[float]:
         pass
 
@@ -65,10 +60,6 @@ class ForwardContract(Contract):
     def __init__(self, underlying: Stock, long_short: LongShort, strike: float, expiry: float) -> None:
         super().__init__(underlying, PutCallFwd.FWD, long_short, strike, expiry)
 
-    def convert_to_generic(self) -> GenericContract:
-        return GenericContract(self.contract_type, self.underlying, self.derivative_type, self.long_short,
-                               self.strike, self.expiry)
-
     def get_timeline(self) -> list[float]:
         return [round(self.expiry, self.timeline_digits)]
 
@@ -85,10 +76,6 @@ class EuropeanContract(Contract):
         if derivative_type not in [PutCallFwd.CALL, PutCallFwd.PUT]:
             self.raise_incorrect_derivative_type_error()
         super().__init__(underlying, derivative_type, long_short, strike, expiry)
-
-    def convert_to_generic(self) -> GenericContract:
-        return GenericContract(self.contract_type, self.underlying, self.derivative_type, self.long_short,
-                               self.strike, self.expiry)
 
     def get_timeline(self) -> list[float]:
         return [round(self.expiry, self.timeline_digits)]
@@ -112,10 +99,6 @@ class AmericanContract(Contract):
             self.raise_incorrect_derivative_type_error()
         super().__init__(underlying, derivative_type, long_short, strike, expiry)
 
-    def convert_to_generic(self) -> GenericContract:
-        return GenericContract(self.contract_type, self.underlying, self.derivative_type, self.long_short,
-                               self.strike, self.expiry)
-
     def get_timeline(self) -> list[float]:
         return [round(self.expiry, self.timeline_digits)]
 
@@ -138,10 +121,6 @@ class EuropeanDigitalContract(Contract):
             self.raise_incorrect_derivative_type_error()
         super().__init__(underlying, derivative_type, long_short, strike, expiry)
 
-    def convert_to_generic(self) -> GenericContract:
-        return GenericContract(self.contract_type, self.underlying, self.derivative_type, self.long_short,
-                               self.strike, self.expiry)
-
     def get_timeline(self) -> list[float]:
         return [round(self.expiry, self.timeline_digits)]
 
@@ -163,10 +142,6 @@ class AsianContract(Contract):
         if derivative_type not in [PutCallFwd.CALL, PutCallFwd.PUT]:
             self.raise_incorrect_derivative_type_error()
         super().__init__(underlying, derivative_type, long_short, strike, expiry, num_mon)
-
-    def convert_to_generic(self) -> GenericContract:
-        return GenericContract(self.contract_type, self.underlying, self.derivative_type, self.long_short,
-                               self.strike, self.expiry, self.num_mon)
 
     def get_timeline(self) -> list[float]:
         return [round(((i+1) / self.num_mon) * self.expiry, self.timeline_digits) for i in range(self.num_mon)]
@@ -201,11 +176,6 @@ class EuropeanBarrierContract(Contract):
         }
         return out
 
-    def convert_to_generic(self) -> GenericContract:
-        return GenericContract(self.contract_type, self.underlying, self.derivative_type, self.long_short,
-                               self.strike, self.expiry, self.num_mon, self.barrier.barrier_level,
-                               self.barrier.up_down, self.barrier.in_out)
-
     def get_timeline(self) -> list[float]:
         return [round(((i+1) / self.num_mon) * self.expiry, self.timeline_digits) for i in range(self.num_mon)]
 
@@ -223,58 +193,6 @@ class EuropeanBarrierContract(Contract):
             return self.direction * mult * max(self.strike - obs[-1], 0)
         else:
             self.raise_incorrect_derivative_type_error()
-
-
-class GenericContract(Contract):
-    def __init__(self, contract_type: str, underlying: Stock, derivative_type: PutCallFwd, long_short: LongShort, strike: float,
-                 expiry: float, num_mon: int = 1, barrier: float = np.nan, up_down: UpDown | None = None,
-                 in_out: InOut | None = None) -> None:
-        supported_deriv_types = (PutCallFwd.CALL, PutCallFwd.PUT, PutCallFwd.FWD)
-        if derivative_type not in supported_deriv_types:
-            self.raise_incorrect_derivative_type_error(supported_deriv_types)
-        super().__init__(underlying, derivative_type, long_short, strike, expiry, num_mon)
-        self.contract_type: str = contract_type
-        self.barrier: Barrier | None = None
-        if barrier != np.nan and up_down is not None and in_out is not None:
-            self.barrier = Barrier(barrier, up_down, in_out)
-
-    def to_dict(self) -> dict[str, any]:
-        out = super().to_dict()
-        if self.barrier is not None:
-            out |= {
-                'barrier': self.barrier.barrier_level,
-                'up_down': self.barrier.up_down,
-                'in_out': self.barrier.in_out
-            }
-        return out
-
-    def convert_to_generic(self) -> GenericContract:
-        return self
-
-    def get_timeline(self) -> list[float]:
-        return [round(((i+1) / self.num_mon) * self.expiry, self.timeline_digits) for i in range(self.num_mon)]
-
-    def payoff(self, spot: dict[float, float]) -> float:
-        timeline = self.get_timeline()
-        if not set(timeline).issubset(set(spot.keys())):
-            self.raise_missing_spot_error(list(spot.keys()))
-        obs = [spot[t] for t in timeline]
-        payoff = mean(obs) - self.strike
-        call_put = 1.0 if self.derivative_type == PutCallFwd.CALL else -1.0
-        if self.contract_type == ForwardContract.get_contract_type():
-            return self.direction * payoff
-        elif self.contract_type == EuropeanDigitalContract.get_contract_type():
-            return self.direction * float(call_put * payoff > 0)
-        elif self.contract_type == EuropeanBarrierContract.get_contract_type():
-            mult = 1.0
-            if self.barrier is not None:
-                in_out = self.barrier.in_out
-                is_breached = int(self.barrier.is_breached(obs))
-                mult = float(int(in_out == InOut.IN) * is_breached + int(in_out == InOut.OUT) * (1 - is_breached))
-            payoff = (obs[-1] - self.strike)
-            return self.direction * mult * max(call_put * payoff, 0)
-        else:
-            return self.direction * max(call_put * payoff, 0)
 
 
 class Barrier:
