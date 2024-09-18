ec9a83303ac7e2449daed29ea9e9fba6c7496c9e
Péter Elek <elekpeter2000@gmail.com>
2024-09-18 13:08:26 +0200

    add checks for create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -7,9 +7,17 @@ if [[ "$pyv" -eq "9" ]]
 then 
     echo "Valid version"
     python3 -m venv --clear venv_quant_course_v39
-    source ./venv_quant_course_v39/bin/activate #lehet a Scriptben van bizonyos distrokon
+    if [[ -f ./venv_quant_course_v39/bin/activate ]]; then 
+        source ./venv_quant_course_v39/bin/activate #lehet a Scriptben van bizonyos distrokon
+    else if [[ -f ./venv_quant_course_v39/Scripts/activate ]]; then
+        source ./venv_quant_course_v39/Scripts/activate
+    else 
+        echo "activate Script not found"
+        return 1
+    fi
+    fi
     python3 -m pip install --upgrade pip
-    pip install -r QuantCourseBP/requirements.txt #ha más útvonalon van a txt azt adjuk meg
+    pip install -r requirements.txt #ha más útvonalon van a txt azt adjuk meg
     ipython kernel install --user --nacd me=venv_quant_course_v39
     jupyter-nbextension install rise --py --sys-prefix
     deactivate
