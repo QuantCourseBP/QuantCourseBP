c8988a00b8075903377238e2dd7bef5fcbac312f
Romsics, Erzsebet <romsics.csore@gmail.com>
2026-02-18 17:35:10 +0100

    random change

--- create_env.bat
+++ create_env.bat
@@ -1,11 +1,11 @@
 @echo off
 :: create virutal env folder
-python -m venv --clear venv_quant_course_v39
+C:\Users\Samyfarha\AppData\Local\Programs\Python\Python39\python.exe -m venv --clear venv_quant_course_v39
 :: activate empty env
 call .\venv_quant_course_v39\Scripts\activate
 :: install required Python packages
 pip install -r requirements.txt
-python -m pip install --upgrade pip
+C:\Users\Samyfarha\AppData\Local\Programs\Python\Python39\python.exe -m pip install --upgrade pip
 :: install jupyter kernel for virtual env
 ipython kernel install --user --name=venv_quant_course_v39
 :: install rise for notebook presentation mode
