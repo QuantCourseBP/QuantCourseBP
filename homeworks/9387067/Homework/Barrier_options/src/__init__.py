5aff73c65dc01bd1e35b6fb2c00eedeb5008b578
Márton Bálint Gyebnár <martongyebnar@gmail.com>
2026-05-10 23:55:10 +0200

    Part a) of Barrier options assignment

---
+++ homeworks/9387067/Homework/Barrier_options/src/__init__.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+
+
+package_location = Path(__file__).parent.parent.resolve()
+package_root = os.path.dirname(__file__)
+
+HOMEWORK_DIR = 'homeworks'
+
+GIT_DATA = {}
+
+
+def _set_git_data():
+    global GIT_DATA
+    import git
+    this_repo = git.Repo(str(package_location))
+    GIT_DATA['repository'] = this_repo.remotes.origin.url
+    try:
+        GIT_DATA['branch'] = this_repo.active_branch.name
+    except:
+        GIT_DATA['branch'] = 'detached_' + this_repo.head.object.hexsha
+
+    quantc_hash = this_repo.commit().hexsha
+    quantc_dirty = '*' if this_repo.is_dirty(untracked_files=True) else ''
+    
+    GIT_DATA['sha'] = f'QuantCourseBP {quantc_hash}{quantc_dirty}'
+    print(GIT_DATA['sha'])
+
+
+_set_git_data()
